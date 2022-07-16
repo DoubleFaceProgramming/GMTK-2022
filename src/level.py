ff68af14af879bc57449eff8ce3cdfdcf7d0bf29
^-^ Trevor <wasandland@outlook.com>
2022-07-16 16:19:20 +0100

    movement break

--- src/level.py
+++ src/level.py
@@ -20,21 +20,20 @@ import time
 class Void(Sprite):
     def update(self) -> None:
         player = self.game.level.player
-        d_pos = player.pos - player.prev_pos # delta_pos
-        new_rect = pygame.Rect(player.prev_pos.x + d_pos.x, player.prev_pos.y, *player.size) # player rect but we move x first
-        if new_rect.colliderect(self.rect):
-            pixel_pos = self.pos * TILE_SIZE # pixel position of this tile
-            if d_pos.x > 0: # left collision pushout
-                player.pos.x = pixel_pos.x - player.size.x
-            elif d_pos.x < 0: # right collision pushout
-                player.pos.x = pixel_pos.x + TILE_SIZE
-        new_rect = pygame.Rect(player.pos.x, player.prev_pos.y + d_pos.y, *player.size) # we move y now
-        if new_rect.colliderect(self.rect):
-            pixel_pos = self.pos * TILE_SIZE # pixel position of this tile
-            if d_pos.y > 0: # top collision pushout
-                player.pos.y = pixel_pos.y - player.size.y
-            elif d_pos.y < 0: # bottom collision pushout
-                player.pos.y = pixel_pos.y + TILE_SIZE
+        d_pos = player.pos - player.prev_pos
+
+        base_pos = newvec(self.pos)
+        r = self.image.get_rect()
+        r.topleft = self.pos * TILE_SIZE - player.camera.offset
+
+        if player.rect.colliderect(r):
+            self.pos += (sign(d_pos.x), sign(d_pos.y))
+
+            print(self.pos, base_pos)
+
+            if isinstance(self.game.level.map[int(self.pos.y)][int(self.pos.x)], Void):
+                player.pos = player.prev_pos
+                self.pos = base_pos
 
 class Floor(Sprite):
     def __init__(self, layer: int | LayersEnum, game: Game, pos: VEC):
@@ -93,6 +92,8 @@ class Dice(Sprite):
                 direction = DIREC.RIGHT
                 self.pos.x += sign(d_pos.x)
 
+            print(type(self.game.level[self.pos]))
+
             if isinstance(self.game.level[self.pos], Void):
                 player.pos = player.prev_pos # TODO: ex. dice on left, hold a, tapping s/w does nothing
                 self.pos = base_pos
@@ -102,12 +103,18 @@ class Dice(Sprite):
                 level = self.game.level
 
                 for pos in positions:
-                    level[pos].kill()
-                    level[pos] = DiceFace(self.game, pos)
+                    try:
+                        level[pos].kill()
+                        # del level[pos]
+                        # if level[pos]
+                        level[pos] = DiceFace(self.game, pos)
+                    except IndexError:
+                        pass # This os fine.
+
 
                 direction = None
-                self.kill()
-                level[self.pos] = None
+                # self.kill()
+                # level[self.pos] = None
 
             if direction:
                 self.roll(direction)
@@ -141,23 +148,32 @@ class Level:
         dice_data = parsed["die"]
         map_data = parsed["map"]
 
+        die_pos = []
+        self.die = []
         self.map = []
         for i, row in enumerate(map_data):
             self.map.append([])
             for j, tile in enumerate(row):
                 key = legend[str(tile)].upper()
                 type_class = SpriteTypes[key].value
-                if type_class != Player:
-                    sprite = type_class(LayersEnum.WORLD, self.game, (j, i))
-                    self.game.sprite_manager.add(sprite)
-                    self.map[i].append(sprite)
-                else:
+                if type_class == Player:
                     player_tile_pos = (j, i)
+                    sprite = Floor(LayersEnum.WORLD, self.game, (j, i))
+                elif type_class == Dice:
+                    die_pos.append((j, i))
+                    sprite = Floor(LayersEnum.WORLD, self.game, (j, i))
+                else:
+                    sprite = type_class(LayersEnum.WORLD, self.game, (j, i))
+                self.game.sprite_manager.add(sprite)
+                self.map[i].append(sprite)
 
         self.player = Player(LayersEnum.MOVEABLES, self.game, player_tile_pos)
+        for pos in die_pos:
+            self.die.append(Dice(LayersEnum.MOVEABLES, self.game, pos))
+
 
     def __getitem__(self, key: VEC):
         return self.map[int(key.y)][int(key.x)]
 
     def __setitem__(self, key: VEC, value: ...):
-        self.map[int(key.y) - 1][int(key.x) - 1] = value
+        self.map[int(key.y)][int(key.x)] = value
